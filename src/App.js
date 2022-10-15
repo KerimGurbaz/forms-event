e8c829d312b62d54f6fdcd9ab853fa17cadfdfff
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-10-16 00:45:59 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 
-// import './App.css';
-// import Form from './components/forms/Form';
-// import FormObject from './components/forms/FormObject';
+import './App.css';
+import Form from './components/forms/Form';
+import FormObject from './components/forms/FormObject';
 import KeyboardClipboard from './components/keyboardClipboardEvent/KeyboardClipboard';
 import MouseEvent from './components/mouseEvents/MouseEvent';
 
 function App() {
   return (
     <div className="container mt-4 text-center ">
-      {/* <Form/> */}
-      {/* <FormObject/> */}
-      {/* <KeyboardClipboard/> */}
+      <Form/>
+      <FormObject/>
+      <KeyboardClipboard/>
       <MouseEvent/>
 
 
